94f7c4e2320092ee3cbb50d46c965c0a65a5a5fd
Nikhilk <nikhil.kanawade@uniken.com>
2020-07-20 14:10:23 +0530

    Plugin migrated to androidX

--- android/src/main/java/com/evollu/react/fcm/MessagingService.java
+++ android/src/main/java/com/evollu/react/fcm/MessagingService.java
@@ -6,7 +6,7 @@ import android.content.pm.ApplicationInfo;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
-import android.support.v4.content.LocalBroadcastManager;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
